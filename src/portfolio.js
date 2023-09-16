f6785df6bd4f1c7794a48e759ecdcebfd176e5f6
Shiyang-Zhao <shiyang2575326696@gmail.com>
2023-09-16 00:59:21 -0700

    Update

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,58 +1,24 @@
 import resumePDF from '../src/files/Shiyang Zhao Resume.pdf'
 import transcriptPDF from '../src/files/Transcript.pdf'
-import pet1 from '../src/files/Pet/pet (1).jpg'
-import pet2 from '../src/files/Pet/pet (2).jpg'
-import pet3 from '../src/files/Pet/pet (3).jpg'
-import pet4 from '../src/files/Pet/pet (4).jpg'
-import pet5 from '../src/files/Pet/pet (5).jpg'
-import pet6 from '../src/files/Pet/pet (6).jpg'
-import pet7 from '../src/files/Pet/pet (7).jpg'
-import pet8 from '../src/files/Pet/pet (8).jpg'
-import pet9 from '../src/files/Pet/pet (9).jpg'
-import pet10 from '../src/files/Pet/pet (10).jpg'
-import volunteer1 from '../src/files/Volunteer/volunteer (1).jpg'
-import volunteer2 from '../src/files/Volunteer/volunteer (2).jpg'
-import volunteer3 from '../src/files/Volunteer/volunteer (3).jpg'
-import volunteer4 from '../src/files/Volunteer/volunteer (4).jpg'
-import volunteer5 from '../src/files/Volunteer/volunteer (5).jpg'
-import volunteer6 from '../src/files/Volunteer/volunteer (6).jpg'
-import travel1 from '../src/files/Travel/travel (1).jpg'
-import travel2 from '../src/files/Travel/travel (2).jpg'
-import travel3 from '../src/files/Travel/travel (3).jpg'
-import travel4 from '../src/files/Travel/travel (4).jpg'
-import travel5 from '../src/files/Travel/travel (5).jpg'
-import travel6 from '../src/files/Travel/travel (6).jpg'
-import travel7 from '../src/files/Travel/travel (7).jpg'
-import travel8 from '../src/files/Travel/travel (8).jpg'
-import travel9 from '../src/files/Travel/travel (9).jpg'
-import travel10 from '../src/files/Travel/travel (10).jpg'
-import travel11 from '../src/files/Travel/travel (11).jpg'
-import travel12 from '../src/files/Travel/travel (12).jpg'
-import travel13 from '../src/files/Travel/travel (13).jpg'
-import travel14 from '../src/files/Travel/travel (14).jpg'
-import travel15 from '../src/files/Travel/travel (15).jpg'
-import travel16 from '../src/files/Travel/travel (16).jpg'
-import travel17 from '../src/files/Travel/travel (17).jpg'
-import travel18 from '../src/files/Travel/travel (18).jpg'
-import hobbies1 from '../src/files/Hobbies/hobbies (1).jpg'
-import hobbies2 from '../src/files/Hobbies/hobbies (2).jpg'
-import hobbies3 from '../src/files/Hobbies/hobbies (3).jpg'
-import hobbies4 from '../src/files/Hobbies/hobbies (4).jpg'
-import hobbies5 from '../src/files/Hobbies/hobbies (5).jpg'
-import hobbies6 from '../src/files/Hobbies/hobbies (6).jpg'
-import hobbies8 from '../src/files/Hobbies/hobbies (8).jpg'
-import hobbies9 from '../src/files/Hobbies/hobbies (9).jpg'
-import hobbies10 from '../src/files/Hobbies/hobbies (10).jpg'
-import hobbies12 from '../src/files/Hobbies/hobbies (12).jpg'
-import hobbies13 from '../src/files/Hobbies/hobbies (13).jpg'
-import hobbies14 from '../src/files/Hobbies/hobbies (14).jpg'
-import hobbies15 from '../src/files/Hobbies/hobbies (15).jpg'
-import hobbies16 from '../src/files/Hobbies/hobbies (16).jpg'
-import hobbies17 from '../src/files/Hobbies/hobbies (17).jpg'
-import hobbies18 from '../src/files/Hobbies/hobbies (18).jpg'
 import profileImageLight from './files/ProfileImage/profileImageLight.jpg'
 import profileImageDark from './files/ProfileImage/profileImageDark.jpg'
 
+const volunteer = [];
+const pet = [];
+
+for (let i = 1; i <= 10; i++) {
+  import(`../src/files/Pet/pet (${i}).jpg`).then(imageModule => {
+    pet.push(imageModule.default);
+    console.log(pet);
+  })
+}
+
+for (let i = 0; i < 10; i++) {
+  import(`../src/files/Pet/pet (${i + 1}).jpg`).then(imageModule => {
+    pet.push(imageModule.default);
+    console.log(pet);
+  })
+}
 
 const header = {
   homepage: '/',
@@ -162,19 +128,20 @@ const skills = [
 const life = [
   {
     name: 'Volunteer',
-    photos: [volunteer1, volunteer2, volunteer3, volunteer4, volunteer5, volunteer6]
+    photos: volunteer
   },
   {
-    name: 'Hobbies',
-    photos: [hobbies1, hobbies2, hobbies3, hobbies4, hobbies5, hobbies6, hobbies8, hobbies9, hobbies10, hobbies12, hobbies13, hobbies14, hobbies15, hobbies16, hobbies17, hobbies18]
+    name: 'Hobby',
+    photos: hobby
+
   },
   {
     name: 'Pet',
-    photos: [pet1, pet2, pet3, pet4, pet5, pet6, pet7, pet8, pet9, pet10]
+    photos: pet
   },
   {
     name: 'Travel',
-    photos: [travel1, travel2, travel3, travel4, travel5, travel6, travel7, travel8, travel9, travel10, travel11, travel12, travel13, travel14, travel15, travel16, travel17, travel18]
+    photos: travel
   }
 ]
 
