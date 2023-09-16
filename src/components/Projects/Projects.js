ab46059d39566728697da5d8f804ce70a207ec3f
Shiyang-Zhao <shiyang2575326696@gmail.com>
2023-09-16 13:14:57 -0700

    Update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,22 +2,24 @@ import uniqid from 'uniqid'
 import { projects } from '../../portfolio'
 import ProjectContainer from '../ProjectContainer/ProjectContainer'
 import './Projects.css'
+import React from 'react'
 
 const Projects = () => {
-  if (!projects.length) return null
 
   return (
-    <section id='projects' className='section projects'>
-      <h2 className='section__title'>Projects</h2>
+    <React.Fragment>
+      {projects && (<section id='projects' className='section projects'>
+        <h2 className='section__title'>Projects</h2>
 
-      <div className='projects__grid'>
-        {projects.map((project) => (
-          <div className='project__item'>
-            <ProjectContainer key={uniqid()} project={project} />
-          </div>
-        ))}
-      </div>
-    </section>
+        <div className='projects__grid'>
+          {projects.map((project) => (
+            <div className='project__item'>
+              <ProjectContainer key={uniqid()} project={project} />
+            </div>
+          ))}
+        </div>
+      </section>)}
+    </React.Fragment>
   )
 }
 
--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,34 +1,36 @@
 import uniqid from 'uniqid'
 import { work } from '../../portfolio'
 import './Work.css'
+import React from 'react'
 
 const Work = () => {
-  if (!work.length) return null
 
   return (
-    <section id='work' className='section work'>
-      <h2 className='section__title'>Work Experience</h2>
+    <React.Fragment>
+      {work && (<section id='work' className='section work'>
+        <h2 className='section__title'>Work Experience</h2>
 
-      <div className='work__grid'>
-        {work.map((job) => (
-          <div key={uniqid()} className='work__item'>
-            <h3 className='work__title'>{job.jobTitle}</h3>
-            <p className='work__company'>{job.company}</p>
-            <p className='work__dates'>{`${job.startDate} - ${job.endDate}`}</p>
-            <p className='work__location'>{job.location}</p>
-            <p className='work__description'>{job.description}</p>
-            <div className='work__skills'>
-              <h4 className='work__skills-title'>Skills:</h4>
-              <ul className='work__skills-list'>
-                {job.skills.map((skill) => (
-                  <li key={uniqid()} className='work__skill'>{skill}</li>
-                ))}
-              </ul>
+        <div className='work__grid'>
+          {work.map((job) => (
+            <div key={uniqid()} className='work__item'>
+              <h3 className='work__title'>{job.jobTitle}</h3>
+              <p className='work__company'>{job.company}</p>
+              <p className='work__dates'>{`${job.startDate} - ${job.endDate}`}</p>
+              <p className='work__location'>{job.location}</p>
+              <p className='work__description'>{job.description}</p>
+              <div className='work__skills'>
+                <h4 className='work__skills-title'>Skills:</h4>
+                <ul className='work__skills-list'>
+                  {job.skills.map((skill) => (
+                    <li key={uniqid()} className='work__skill'>{skill}</li>
+                  ))}
+                </ul>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
-    </section>
+          ))}
+        </div>
+      </section>)}
+    </React.Fragment>
   )
 }
 
