9502ede4c0c1d692493ecfd995901cccb9c14b89
Shiyang-Zhao <shiyang2575326696@gmail.com>
2023-10-01 21:43:10 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ const App = () => {
       <Header />
 
       <main>
-        <Background />
+        {/* <Background /> */}
         <FadeInSection>
           <About />
         </FadeInSection>
